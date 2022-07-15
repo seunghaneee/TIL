4edc617f7bfedac8ba687d5490a1230a83ce10fc
seunghaneee <seunghan4769@pukyong.ac.kr>
2022-07-15 14:39:38 +0900

    SC day2 | python intro

---
+++ startcamp/age.py
@@ -0,0 +1,11 @@
+from urllib import response
+import requests
+
+url = 'https://api.agify.io?name=michael'
+response = requests.get(url).json()
+print(type(response))
+age = response['age']
+name = response['name']
+count = response['count']
+
+print('이름이' + name + '인 사람의 나이는' + str(age)+ '입니다.')
